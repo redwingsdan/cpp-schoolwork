08375fac86d71fbc01e6cebc2f244116feb6c307
redwingsdan <djp21795@yahoo.com>
2020-08-04 00:06:52 -0400

    Update Main.cpp

--- TESTING THING/TESTING THING/Main.cpp
+++ TESTING THING/TESTING THING/Main.cpp
@@ -8,7 +8,7 @@ int main()
 
 	cout << "Enter the temperature in celsius: ";
 	cin >> c;
-	f=((c + (9/5)) +32);
+	f=((c * (9/5)) +32);
 	cout << "The temperature in farenheit is: "
 		<< f << endl;
 	cin.get();
